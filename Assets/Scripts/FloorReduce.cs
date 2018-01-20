65cc0d01d9c0bcbb682cfc792ffa6879f085f352
aayushsharma9 <aayush.sharma317@gmail.com>
2018-01-20 16:19:02 +0530

    Added Project Files
    Added the unity project folder.

---
+++ Assets/Scripts/FloorReduce.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FloorReduce : MonoBehaviour
+{
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Energy")
+        {   
+            //Debug.Log("GAME OVER");
+            Destroy(other.gameObject);
+            GameManager.EnergyLevel--;
+        }
+    }
+}
---
+++ Assets/Scripts/PickBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PickBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float speed, acceleration;
+
+    private void Start()
+    {
+        
+    }
+
+    public void SetSpeedAndAcceleration(int _speed, int _acc)
+    {
+        speed = _speed;
+        acceleration = _acc;
+    }
+}
